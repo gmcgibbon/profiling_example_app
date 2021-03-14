56392d47eb8797f67aa5e070dbff8d53af82df97
Gannon McGibbon <gannon.mcgibbon@shopify.com>
2021-03-14 05:40:48 -0400

    Authorize production profiling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize_profile
+
   private
+
+  def authorize_profile
+    Rack::MiniProfiler.authorize_request if can_profile?
+  end
+
+  def can_profile?
+    request.ip == "127.0.0.1"
+  end
 
   def set_cart
     @cart = Cart.find_by(id: session[:cart_id] ||= Cart.create!.id)
