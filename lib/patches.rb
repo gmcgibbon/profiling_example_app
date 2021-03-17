03fa2302bd2522026f8ebe20957a239ab6dcb399
Gannon McGibbon <gannon.mcgibbon@shopify.com>
2021-03-17 06:23:37 -0400

    Use load callbacks for patches

--- lib/patches.rb
+++ lib/patches.rb
@@ -1,16 +1,16 @@
-Rails.application.reloader.to_prepare do
+Rails.autoloaders.main.on_load("ActiveStorage::AnalyzeJob") do
   ActiveStorage::AnalyzeJob.class_eval do
     def nothing_to_see_here_🙈
     end
   end
 end
 
-ActionController::Base.class_eval do
+ActiveSupport.on_load(:action_controller) do
   def super_cool_method_😎
   end
 end
 
-ActiveRecord::Base.class_eval do
+ActiveSupport.on_load(:active_record) do
   def i_like_emojis_✨
   end
 end
